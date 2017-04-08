65c2c3a092261e9bc760f163405b6bd12b98e4a4
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-04-07 17:05:02 -0700

    DRY up code

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,24 +1,14 @@
 require 'semantics3'
 
-class ProductSearch < ActiveRecord::Base
-
-  def setup
-    @sem3 = Semantics3::Products.new(Rails.application.secrets.sem3_api_key,
-                                     Rails.application.secrets.sem3_api_secret)
-  end
-
-  def construct_query
-    @sem3.products_field('search', query)
-  end
+class ProductSearch < ApplicationRecord
 
   def get_products
+    #CACHING!!!!!!!!!!!!!! 😎
     results || create_results
   end
 
   def create_results
-    setup
-    construct_query
-    results = @sem3.get_products["results"]
+    results = Sem3SearchService.new({search: query}).get_products["results"]
     update(results: results)
     results
    end
--- app/services/sem3_search_service.rb
+++ app/services/sem3_search_service.rb
@@ -27,23 +27,4 @@ class Sem3SearchService
   def get_products
     @sem3.get_products
   end
-
-  def store
-    productsHash = get_products
-    products_json = productsHash.to_json
-
-    products = products_json['results'].map do |item|
-      p = ProductSearch.new
-      p.name = item['name']
-      p.image_url = item['images'].map { |url| url }
-      p.description = item['description']
-      p.brand = item['brand']
-      p.price = item['price']
-
-      p.save
-      p
-    end
-    products.select(&:persisted?)
-
-  end
 end
